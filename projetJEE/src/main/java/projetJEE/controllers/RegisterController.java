77acbea3fa0edba9cfb578b385a3f78ab09a2c7f
= <remito@live.fr>
2019-04-04 12:15:54 +0200

    register

--- projetJEE/src/main/java/projetJEE/controllers/RegisterController.java
+++ projetJEE/src/main/java/projetJEE/controllers/RegisterController.java
@@ -17,12 +17,8 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import projetJEE.bl.concrete.AddressManager;
-import projetJEE.bl.concrete.TypeManager;
-import projetJEE.bl.concrete.UserAccountManager;
-import projetJEE.models.Address;
-import projetJEE.models.Type;
-import projetJEE.models.UserAccount;
+import projetJEE.bl.concrete.*;
+import projetJEE.models.*;
 
 @Controller public class RegisterController {
     private static final Logger logger = Logger.getLogger(DefaultController.class);
@@ -32,6 +28,10 @@ import projetJEE.models.UserAccount;
     AddressManager adrmanager;
     @Resource
     TypeManager typmanager;
+    @Resource
+    CountryManager countryManager;
+    @Resource
+    TypeManager typeManager;
     
     @RequestMapping(value = "/Register", method = RequestMethod.GET)
     public String registerPage(ModelMap map) {
@@ -47,11 +47,11 @@ import projetJEE.models.UserAccount;
           @RequestParam(value="password", required=false) String password,
           @RequestParam(value="password2", required=false) String password2,
           @RequestParam(value="phoneNumber", required=false) String phoneNumber,
-          @RequestParam(value="address", required=false) String address,
+          @RequestParam(value="street", required=false) String street,
           @RequestParam(value="city", required=false) String city,
           @RequestParam(value="zipCode", required=false) String zipCode,
           @RequestParam(value="state", required=false) String state,
-          @RequestParam(value="contry", required=false) String contry,
+          @RequestParam(value="country", required=false) String country,
           ModelMap map){
       
         logger.info("Accès à la page de register. POST");
@@ -64,11 +64,11 @@ import projetJEE.models.UserAccount;
         password
         password2
         phoneNumber
-        address
+        street
         city
         zipCode
         state
-        contry
+        country
         */
         
         try {
@@ -89,7 +89,7 @@ import projetJEE.models.UserAccount;
                     throw new Exception("Le champ password doit être complété.");
             }
             
-            if(password.equals(password)) {
+            if(!password.equals(password2)) {
                     throw new Exception("Les deux mots de passes ne sont pas identiques.");
             }
             
@@ -97,7 +97,7 @@ import projetJEE.models.UserAccount;
                     throw new Exception("Le champ phoneNumber doit être complété.");
             }
             
-            if(address == null || address.isEmpty()) {
+            if(street == null || street.isEmpty()) {
                     throw new Exception("Le champ address doit être complété.");
             }
             
@@ -113,36 +113,51 @@ import projetJEE.models.UserAccount;
                     throw new Exception("Le champ state doit être complété.");
             }
             
-            if(contry == null || contry.isEmpty()) {
+            if(country == null || country.isEmpty()) {
                     throw new Exception("Le champ contry doit être complété.");
             }
             
-            List<UserAccount> user = uamanager.getAllUsers();
-           /* 
-            if(uamanager.validateLogin(email, password))
+            // test email exist
+
+            // get country if exist
+            Country objtCountry = null;//getCountryById(contry);
+            if(objtCountry == null) // so add country
             {
-                LocalDate now = LocalDate.now();
-                UserAccount newUser = UserAccount(firstName, lastName, email, password, phoneNumber, false, now, now, null, null, false, Type type, Address address)
-                
-                    // la connexion est établie
-                    session.setAttribute("user", email);
-                    logger.info("La connexion est établie en tant que " + email);
-                    return "index"; 
+                objtCountry = new Country(country);
+                countryManager.addCountry(objtCountry);
             }
-            else
-            {
-                    // connexion refusée
-                    throw new Exception("Le pseudo ou le mot de passe n'est pas correcte !");
-            }
-*/return "index"; 
+
+            
+            // add a new adress
+            Address address = new Address(street, city, state, zipCode, objtCountry);
+            adrmanager.addAddress(address);
+
+            Type type = typeManager.getTypeByName("client");
+
+            // add user account
+            LocalDate now = LocalDate.now();
+            System.out.println("Before add account");
+            UserAccount newUser = new UserAccount(firstName, lastName, email, password, phoneNumber, false, now, now, "fff", null, false, type, address);
+            
+           System.out.println("After create object");
+            uamanager.addUserAccount(newUser);
+System.out.println("After add account");
+           return "index"; 
+            
         } catch(Exception e) {
-
+                request.setAttribute("firstName", firstName);
+                request.setAttribute("lastName", lastName);
                 request.setAttribute("email", email);
-                request.setAttribute("password", password);
-
+                request.setAttribute("phoneNumber", phoneNumber);
+                request.setAttribute("street", street);
+                request.setAttribute("city", city);
+                request.setAttribute("zipCode", zipCode);
+                request.setAttribute("state", state);
+                request.setAttribute("country", country);
+                
                 request.setAttribute("erreur", e.getMessage());
                 logger.info("Connexion refusé");
-                return "login"; 
+                return "registerPage"; 
         }
   }
   
--- projetJEE/src/main/java/projetJEE/domain/repository/TypeRepository.java
+++ projetJEE/src/main/java/projetJEE/domain/repository/TypeRepository.java
@@ -5,9 +5,14 @@
  */
 package projetJEE.domain.repository;
 
+import java.util.List;
 import projetJEE.models.Type;
+import projetJEE.models.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TypeRepository extends JpaRepository<Type, Integer> {
-    
+    @Query("SELECT t FROM Type t WHERE t.type = :nameType")
+    public Type findTypeByName(@Param("nameType") String nameType);
 }
