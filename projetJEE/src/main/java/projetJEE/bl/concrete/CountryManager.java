61e765c4d0738195824538be378a73459692d727
Guillaume <chevallierguillaume97@gmail.com>
2019-04-04 11:29:26 +0200

    fonctions bdd

--- projetJEE/src/main/java/projetJEE/bl/concrete/CountryManager.java
+++ projetJEE/src/main/java/projetJEE/bl/concrete/CountryManager.java
@@ -5,6 +5,7 @@
  */
 package projetJEE.bl.concrete;
 
+import java.util.List;
 import projetJEE.models.Country;
 import projetJEE.domain.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,14 @@ public class CountryManager {
     
     public Country getCountryById(int id) {
         return this.repo.findById(id).get();
+    }
+    
+    public Country getCountryByName(String name){
+        List<Country> foundCountries = this.repo.findByName(name);
+        if(!foundCountries.isEmpty())
+            return foundCountries.get(0);
+        else
+            return null;
     }
     
     public void addCountry(Country country){
--- projetJEE/src/main/java/projetJEE/domain/repository/CountryRepository.java
+++ projetJEE/src/main/java/projetJEE/domain/repository/CountryRepository.java
@@ -5,9 +5,14 @@
  */
 package projetJEE.domain.repository;
 
+import java.util.List;
 import projetJEE.models.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CountryRepository extends JpaRepository<Country, Integer> {
-    
+    @Query("SELECT c FROM Country c WHERE c.country = :name")
+    public List<Country> findByName(@Param("name") String name);
 }
+
